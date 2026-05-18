0951cd3d7b1b03f54a145b8eda3f40aa7415dde3
Tio pratama agung <tio.agung@koltiva.com>
2026-05-18 14:51:49 +0700

    added report by json and new config for report generate by json

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,10 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+  ['html'],
+    ['json', { outputFile: 'reports/results.json'}]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
      headless: false, 
